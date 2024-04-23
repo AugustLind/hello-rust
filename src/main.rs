20ced09c6bdc53084329c8701bb1677ddd8b1ece
August <August04lind@gmail.com>
2024-04-23 19:01:08 +0200

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    let secret = rand::thread_rng().gen_range(1..=100);
+    let mut guess = String::new();
+
+    loop {
+        guess.clear();
+        println!("What is your guess?");
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            },
+        };
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Equal => {
+                println!("Correct");
+                break;
+            },
+            Ordering::Greater => println!("Too big"),
+        }
+    }
 }
